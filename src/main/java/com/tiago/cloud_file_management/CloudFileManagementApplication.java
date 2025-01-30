c926fcc20d39cad63a4cf556c82c2f811dae1dd0
Tiago Ledur <tiagoasledur@gmail.com>
2025-01-30 13:44:24 -0300

    Commit inicial

---
+++ src/main/java/com/tiago/cloud_file_management/CloudFileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.cloud_file_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileManagementApplication.class, args);
+	}
+
+}
