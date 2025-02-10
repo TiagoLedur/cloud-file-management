408be06a0995a8380f66fdcb574de7180887a333
Tiago Ledur <tiagoasledur@gmail.com>
2025-02-10 16:49:07 -0300

    Movido para pasta Config

--- src/main/java/com/tiago/cloud_file_management/config/S3Config.java
+++ src/main/java/com/tiago/cloud_file_management/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.tiago.cloud_file_management;
+package com.tiago.cloud_file_management.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
